75750397750064435eee209450cb11ba37893d29
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-27 23:58:08 -0800

    prototypes added

--- main.h
+++ main.h
@@ -14,4 +14,9 @@ typedef struct {
 
 int _printf(const char *format, ...);
 
+void print_char(va_list args, int *count);
+void print_string(va_list args, int *count);
+void print_percent(va_list args, int *count);
+
+
 #endif
