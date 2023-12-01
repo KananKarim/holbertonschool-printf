528e6e08450dfd84c33304662cd5ad51c793e828
KananKarim <kenankerimli7770@gmail.com>
2023-11-30 22:21:21 -0800

    first task

--- main.h
+++ main.h
@@ -1,24 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+
 
 /**
- * struct print_t - Struct for format specifiers and their corresponding functions
- * @specifier: The format specifier
- * @print_func: The function to handle the format specifier
+ * struct prints - Structure representing a printing specifier
+ * @specifier: The format specifier associated with the printing function.
+ * @print_func: Pointer to the function responsible
  */
 
-typedef struct {
-    char *specifier;
-    void (*print_func)(va_list args, int *count);
+typedef struct prints
+{
+	char *specifier;
+	void (*print_func)(va_list args, int *count);
 } print_t;
 
 int _printf(const char *format, ...);
-
 
 int _putchar(char c);
 void print_char(va_list args, int *count);
 void print_string(va_list args, int *count);
 void print_percent(va_list args, int *count);
-void print_int(va_list args, int *count);
 
 #endif
--- printf.c
+++ printf.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_char - Prints a character
+ * @args: A va_list pointing to the character to be printed
+ * @count: will be incremented by the number of characters printed
+ */
+
 void print_char(va_list args, int *count)
 {
 	int c = va_arg(args, int);
@@ -9,6 +15,12 @@ void print_char(va_list args, int *count)
 	_putchar(c);
 	(*count)++;
 }
+
+/**
+ * print_string - Prints a string
+ * @args: A va_list pointing to the string to be printed
+ * @count: will be incremented by the number of characters printed
+ */
 
 void print_string(va_list args, int *count)
 {
@@ -22,9 +34,62 @@ void print_string(va_list args, int *count)
 		(*count)++;
 	}
 }
+
+/**
+ * print_percent - Prints a percent symbol
+ * @args: A va_list that is not used in this function
+ * @count: will be incremented by the number of characters printed
+ */
+
 void print_percent(va_list args, int *count)
 {
 	(void)args;
 	_putchar('%');
 	(*count)++;
+}
+
+/**
+ * _printf - A printf-like function that prints a formatted string
+ * @format: The format string
+ * Return: The number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int i;
+	int count1;
+	const char *traverse;
+	print_t prints[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	va_list args;
+
+	va_start(args, format);
+
+	for (traverse = format; *traverse != '\0'; traverse++)
+	{
+		while (*traverse != '%' && *traverse != '\0')
+		{
+			_putchar(*traverse);
+			traverse++;
+			count1++;
+		}
+		if (*traverse == '\0')
+			break;
+		traverse++;
+		for (i = 0; prints[i].specifier != NULL; i++)
+		{
+			if (*traverse == *(prints[i]).specifier)
+			{
+				prints[i].print_func(args, &count1);
+				break;
+			}
+		}
+	}
+
+	va_end(args);
+	return (count1);
 }
